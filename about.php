e68f0e3e0582db8ebe8ac6ce15ba167a988484a0
Sathwika <114393608+Sathwika1103@users.noreply.github.com>
2023-04-13 14:57:12 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,129 @@
+<?php 
+
+	//include header file
+	include ('include/header.php');
+
+?>
+
+<style>
+	.size{
+		padding: 80px 0px;
+	}
+	img{
+		width: 300px;
+		height: 300px;
+	}
+	h2{
+		color: #e74c3c;
+	}
+	.white{
+		background-color: white;
+	}
+	p{
+		font-size: 18px;
+	}
+	.right{
+		float: right;
+	}
+	h1{
+		color: white;
+	}
+	.size{
+		min-height: 0px;
+		padding: 60px 0 40px 0;
+		
+	}
+
+</style>
+<div class="container-fluid red-background size">
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<h1 class="text-center">About</h1>
+			<hr class="white-bar">
+		</div>
+	</div>
+</div>
+
+<!-- donate section -->
+			<div class="container-fluid red-background">
+				<div class="row">
+					<div class="col-md-12">
+						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">BLOODLINE</h1>
+						<hr class="white-bar">
+						<p class="text-center pera-text">
+							A website to help people in need of blood.
+							The main aim of this project is to help the people who need blood in an
+        emergency and to associate some donors who are willing to donate their
+        blood to needy people and save their lives.It will help us to find the blood group with the most efficient time to take care
+of the blood and it is more easy to hand over the blood to help people to get
+blood on time.
+						</p>
+						<a href="#" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
+					</div>
+				</div>
+			</div>
+			<!-- end doante section -->
+			
+<div class="container-fluid size">
+	
+	<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="img/binoculars.png" alt="Our Vission" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h2 class="text-center">WHY DONATE BLOOD</h2>
+			<hr class="red-bar">
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+		</div>
+	</div>
+	</div>
+</div>
+
+<div class="container-fluid white size">
+	<div class="container ">
+	<div class="container">
+		<div class="row ">
+		<div class="col-md-6">
+			<h2 class="text-center">CAN YOU DONATE</h2>
+			<hr class="red-bar">
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+		</div>
+		<div ><img src="img/target.png" alt="Our Vission" class="rounded img-fluid float-right"></div>
+	</div>
+	</div>		
+</div>
+</div>
+	
+
+	<div class="container-fluid size">
+		<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="img/goal.png" alt="Our Vission" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h2 class="text-center">Who can't donate blood</h2>
+			<hr class="red-bar">
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+			<p>
+				We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+			</p>
+		</div>
+	</div>
+	</div>
+	</div>
+<?php 
+
+	//include footer file
+	include ('include/footer.php');
+
+?>
---
+++ include/footer.php
@@ -0,0 +1,33 @@
+<!-- contact us section -->
+			<div class="container-fluid">					
+				<div class="row contactus">
+					<div class="col-md-6 offset-md-3">
+						<h1 class="text-center">Contact Us</h1>
+						<hr class="white-bar">
+
+							
+							<div class="details">
+								 <p>+91 9666469644 <i class="fa fa-phone"></i>
+								  gandyadapu.1@iitj.ac.in <i class="fa fa-envelope"></i>  </p>
+							</div>
+							<div class="details">
+								 <p>+91 9603632976 <i class="fa fa-phone"></i>
+								  gavva.1@iitj.ac.in <i class="fa fa-envelope"></i>  </p>
+							</div>
+
+					</div>
+				</div>
+			</div>
+			<!-- end contact us section -->
+			
+	    <!-- JQuery File -->
+    <script type="text/javascript" src="jquery/jquery.js"></script>
+    
+    <!-- BootStrap JS File-->
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Fontawesome Icon JS-->
+    <script defer src="fontawesome/js/all.js"></script>
+
+	</body>
+</html>
